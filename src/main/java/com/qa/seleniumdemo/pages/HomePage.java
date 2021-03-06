e91fddbc3cf5ca60a9449793be1e11d9af4e0233
Sunil Kumar <Sunil.K@marketcube.com>
2021-03-06 18:00:59 +0530

    constructor and homepagetext method add in HomePage

--- src/main/java/com/qa/seleniumdemo/pages/HomePage.java
+++ src/main/java/com/qa/seleniumdemo/pages/HomePage.java
@@ -1,5 +1,17 @@
 package com.qa.seleniumdemo.pages;
 
+import org.openqa.selenium.WebDriver;
+
 public class HomePage {
 
+	WebDriver driver;
+	
+	public HomePage(WebDriver driver) {
+		this.driver=driver;
+	}
+	
+	public void homepageText() {
+		System.out.println("Home Page");
+	}
+	
 }
