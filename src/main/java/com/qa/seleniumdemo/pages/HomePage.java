4533154eff6e98db72bd24bc2fc5621d105eabf8
Sunil Kumar <Sunil.K@marketcube.com>
2021-03-06 18:35:45 +0530

    remove constructor from home page

--- src/main/java/com/qa/seleniumdemo/pages/HomePage.java
+++ src/main/java/com/qa/seleniumdemo/pages/HomePage.java
@@ -6,10 +6,6 @@ public class HomePage {
 
 	WebDriver driver;
 	
-	public HomePage(WebDriver driver) {
-		this.driver=driver;
-	}
-	
 	public void homepageText() {
 		System.out.println("Home Page");
 	}
