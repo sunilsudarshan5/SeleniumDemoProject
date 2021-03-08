31d5743a7f3a5f3414738739d003108eae52a81d
Sunil Kumar <Sunil.K@marketcube.com>
2021-03-08 08:24:07 +0530

    add new method abc test in Home Test class

--- src/test/java/com/qa/seleniumdemo/pagestestcase/HomePageTest.java
+++ src/test/java/com/qa/seleniumdemo/pagestestcase/HomePageTest.java
@@ -1,5 +1,12 @@
 package com.qa.seleniumdemo.pagestestcase;
 
+import org.testng.annotations.Test;
+
 public class HomePageTest {
+	
+	@Test
+	public void abcTest() {
+		System.out.println("Hello Test");
+	}
 
 }
